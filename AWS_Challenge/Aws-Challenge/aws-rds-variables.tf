5cff52056540abeaa2331733bbfc327bfa8af823
velpuriv <107247731+velpuriv@users.noreply.github.com>
2025-03-01 01:49:04 +0530

    Delete AWS_Challenge/Aws-Challenge directory

--- AWS_Challenge/Aws-Challenge/aws-rds-variables.tf
+++
@@ -1,5 +0,0 @@
-variable "key_name" {
-  description = "Existing Key Pair name for SSH access"
-  type        = string
-  default     = "eks-terraform-key"
-}
--- AWS_Challenge/Aws-Challenge/terraform-backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-us-west-2"
-    key            = "terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
-  }
-}
